7f801988673ab03ebe71186bb12e75d20690309e
niketchandra <niket.chandrawanshi@outlook.com>
2025-05-15 19:47:38 +0530

    remove uptime code base

--- uptime/themes/altum/views/s/partials/ads_header.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(
-    !empty(settings()->ads->header_status_pages)
-    && !$this->status_page_user->plan_settings->no_ads
-    && \Altum\Router::$controller_settings['ads']
-): ?>
-    <div class="container my-3 d-print-none"><?= settings()->ads->header_status_pages ?></div>
-<?php endif ?>
--- uptime/update/sql/3500.sql
+++
@@ -1,3 +0,0 @@
-UPDATE `settings` SET `value` = '{\"version\":\"35.0.0\", \"code\":\"3500\"}' WHERE `key` = 'product_info';
-
--- SEPARATOR --
